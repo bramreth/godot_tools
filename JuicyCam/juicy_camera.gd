ea03a2bb3f116a515e2192da2d74df9d9b3957e2
Bram <bramreth@gmail.com>
2021-05-15 09:34:04 +0100

    curve tween and juicy cam script imptovements

--- JuicyCam/juicy_camera.gd
+++ JuicyCam/juicy_camera.gd
@@ -1,26 +1,27 @@
 extends Camera2D
+class_name JuicyCamera
 
-export (OpenSimplexNoise) var noise
-export(float, 0, 1) var trauma = 0.0
+export var noise: OpenSimplexNoise
+export var trauma = 0.0 setget set_trauma
 
-export var max_x = 150
-export var max_y = 150
-export var max_r = 25
+export var max_x := 33
+export var max_y := 33
+export var max_r := 5
 
-export var time_scale = 150
+export var time_scale = 20
 
 export(float, 0, 1) var decay = 0.6
 
-var time = 0
+var time := 0.0
 
-func add_trauma(trauma_in):
+func set_trauma(trauma_in: float) -> void:
 	trauma = clamp(trauma + trauma_in, 0, 1)
+	set_process(not is_zero_approx(trauma))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	time += delta
+func _process(delta: float) -> void:
+	time += delta * time_scale
 	var shake = pow(trauma, 2)
-	offset.x = noise.get_noise_3d(time * time_scale, 0, 0) * max_x * shake
-	offset.y = noise.get_noise_3d(0, time * time_scale, 0) * max_y * shake
-	rotation_degrees = noise.get_noise_3d(0, 0, time * time_scale) * max_r * shake
-	if trauma > 0: trauma = clamp(trauma - (delta * decay), 0, 1)
+	offset.x = noise.get_noise_3d(time, 0, 0) * max_x * shake
+	offset.y = noise.get_noise_3d(0, time, 0) * max_y * shake
+	rotation_degrees = noise.get_noise_3d(0, 0, time) * max_r * shake
+	trauma = clamp(trauma - (delta * decay), 0, 1)
